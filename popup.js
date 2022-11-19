e51c886e08c12f76ed636c0c9174f651fa8d5115
EomHyeYeong <hy30051@naver.com>
2022-11-19 19:07:29 +0900

    url 가져오기 확인

--- popup.js
+++ popup.js
@@ -8,11 +8,10 @@ chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     // alert(typeof(String(domain)))
     var r_domain = String(domain[2]).substring(4);
     alert(r_domain); 
-});
 
     var url = 'http://apis.data.go.kr/B551505/whois/domain_name'; /*URL*/
     var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'cUWf07DL4LpO9A5FA1fWq3vYGsYMKLO1bKVkyYJX1rWeCGciZpn1kyP7l5nwuTfckSvUUuMsdW4f7YuKHrUX%2Bw%3D%3D'; /*Service Key*/
-    queryParams += '&' + encodeURIComponent('query') + '=' + encodeURIComponent('asan.go.kr'); /*도메인 이름, http://... 형식 X*/
+    queryParams += '&' + encodeURIComponent('query') + '=' + encodeURIComponent(r_domain); /*도메인 이름, http://... 형식 X*/
     queryParams += '&' + encodeURIComponent('answer') + '=' + encodeURIComponent('xml'); /**/
     xhr.open('GET', url + queryParams);
     xhr.onreadystatechange = function () {
@@ -22,3 +21,4 @@ chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     };
 
     xhr.send('');
+});
