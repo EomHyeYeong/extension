e4e33e8fe1cfe255d50bc964860f646c62f152da
엄혜영 <112528738+EomHyeYeong@users.noreply.github.com>
2022-11-19 19:32:07 +0900

    서비스키 수정

--- popup copy.js
+++ popup copy.js
@@ -2,7 +2,7 @@
 
 var xhr = new XMLHttpRequest();
     var url = 'http://apis.data.go.kr/B551505/whois/domain_name'; /*URL*/
-    var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'cUWf07DL4LpO9A5FA1fWq3vYGsYMKLO1bKVkyYJX1rWeCGciZpn1kyP7l5nwuTfckSvUUuMsdW4f7YuKHrUX%2Bw%3D%3D'; /*Service Key*/
+    var queryParams = '?' + encodeURIComponent('serviceKey') + '='+''; /*Service Key*/
     queryParams += '&' + encodeURIComponent('query') + '=' + encodeURIComponent('asan.go.kr'); /*도메인 이름, http://... 형식 X*/
     queryParams += '&' + encodeURIComponent('answer') + '=' + encodeURIComponent('xml'); /**/
     xhr.open('GET', url + queryParams);
