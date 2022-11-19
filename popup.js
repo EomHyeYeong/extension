12e5ef5f278a65211f901c52d8d4b804eecbcf20
junsu1220 <crab7157@gmail.com>
2022-11-19 18:42:56 +0900

    문자열 자르기

--- popup.js
+++ popup.js
@@ -2,12 +2,14 @@
 
 var xhr = new XMLHttpRequest();
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    let url = tabs[0].url;
+    let url2 = tabs[0].url;
     // use `url` here inside the callback because it's asynchronous!
-    alert(url);
-
+    var domain = url2.split("/");
+    // alert(typeof(String(domain)))
+    var r_domain = String(domain[2]).substring(4);
+    alert(r_domain); 
 });
-    alert(url);
+
     var url = 'http://apis.data.go.kr/B551505/whois/domain_name'; /*URL*/
     var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'cUWf07DL4LpO9A5FA1fWq3vYGsYMKLO1bKVkyYJX1rWeCGciZpn1kyP7l5nwuTfckSvUUuMsdW4f7YuKHrUX%2Bw%3D%3D'; /*Service Key*/
     queryParams += '&' + encodeURIComponent('query') + '=' + encodeURIComponent('asan.go.kr'); /*도메인 이름, http://... 형식 X*/
